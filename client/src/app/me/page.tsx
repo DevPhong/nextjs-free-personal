4a2305ff3721b68c82a112e456a0f12421bf3eb5
DevPhong <phonghoangle96@gmail.com>
2025-05-23 16:02:22 +0700

    feat/video_19 : Tạo Http client với fetch để gọi API cho Next.js

--- client/src/app/me/page.tsx
+++ client/src/app/me/page.tsx
@@ -1,33 +1,13 @@
 import React from "react";
-import envConfig from "@/config";
 import { cookies } from "next/headers";
 import Profile from "@/app/me/profile";
+import accountApiRequest from "@/apiRequests/account";
 
 export default async function MeProfile() {
-  const cookieStore = cookies();
-  const sessionToken = (await cookieStore).get("sessionToken")?.value;
-
-  const result = await fetch(
-    `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${sessionToken}`,
-      },
-    }
-  ).then(async (res) => {
-    const payload = await res.json();
-    const data = {
-      status: res.status,
-      payload,
-    };
-
-    if (!res.ok) {
-      throw data;
-    }
-
-    return data;
-  });
+  const cookieStore = await cookies();
+  // token này là lấy từ server
+  const sessionToken = cookieStore.get("sessionToken");
+  const result = await accountApiRequest.me(sessionToken?.value ?? "");
 
   return (
     <div>
--- client/src/app/me/profile.tsx
+++ client/src/app/me/profile.tsx
@@ -1,41 +1,20 @@
 "use client";
 
 import React, { useEffect } from "react";
-import envConfig from "@/config";
-import { useAppContext } from "@/AppProvider";
+import accountApiRequest from "@/apiRequests/account";
 
 export default function Profile() {
-  const { sessionToken } = useAppContext();
-
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(
-        `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${sessionToken}`,
-          },
-        }
-      ).then(async (res) => {
-        const payload = await res.json();
-        const data = {
-          status: res.status,
-          payload,
-        };
-
-        if (!res.ok) {
-          throw data;
-        }
-
-        return data;
-      });
-      console.log("result", result);
+    const fetchRequest = async () => {
+      try {
+        const result = await accountApiRequest.meClient();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
     };
-
-    // Gửi dữ liệu lên server nextjs (set-token)
-    fetchData();
-  }, [sessionToken]);
+    fetchRequest();
+  }, []);
 
   return <div>profile</div>;
 }
