dc967456d1f086d0d060be6d70134c27fcde00c6
DevPhong <phonghoangle96@gmail.com>
2025-06-12 11:40:28 +0700

    feat/video_26 - Upload hình ảnh và tạo sản phẩm

---
+++ client/src/app/products/add/product-add-form.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { handleErrorApi } from "@/lib/utils";
+import { useRef, useState } from "react";
+import {
+  CreateProductBody,
+  CreateProductBodyType,
+} from "@/schemaValidations/product.schema";
+import productApiRequest from "@/apiRequests/product";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+
+export default function ProductAddForm() {
+  const form = useForm<CreateProductBodyType>({
+    resolver: zodResolver(CreateProductBody),
+    defaultValues: {
+      name: "",
+      price: 0,
+      description: "",
+      image: "",
+    },
+  });
+  const router = useRouter();
+  const [file, setFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const onSubmit = async (values: CreateProductBodyType) => {
+    if (loading) return; // Ngăn chặn gửi form khi đang xử lý
+    setLoading(true);
+    try {
+      const formData = new FormData();
+      formData.append("file", file as Blob);
+      const uploadImageResult = await productApiRequest.uploadImage(formData);
+      const imageUrl = uploadImageResult.payload.data;
+      const result = await productApiRequest.create({
+        ...values,
+        image: imageUrl,
+      });
+
+      toast.success("", { description: result.payload.message });
+      router.push("/products");
+    } catch (error: any) {
+      // Xử lý lỗi
+      handleErrorApi({
+        error,
+        setError: form.setError,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 max-w-[600px] flex-shrink-0 w-full"
+        noValidate
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Tên sản phẩm</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="text" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Giá sản phẩm</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="number" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mô tả</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Mô tả sản phẩm" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Hình ảnh</FormLabel>
+              <FormControl>
+                <Input
+                  type="file"
+                  accept="image/*"
+                  ref={inputRef}
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    if (file) {
+                      setFile(file);
+                      field.onChange("http://localhost:3000/" + file.name);
+                    }
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {file && (
+          <div>
+            <Image
+              src={URL.createObjectURL(file)}
+              width={128}
+              height={128}
+              alt="preview"
+              className="w-32 h-32 object-cover"
+            />
+            <Button
+              type="button"
+              variant={"destructive"}
+              size={"sm"}
+              onClick={() => {
+                setFile(null);
+                form.setValue("image", "");
+                if (inputRef.current) {
+                  inputRef.current.value = "";
+                }
+              }}
+            >
+              Xóa hình ảnh
+            </Button>
+          </div>
+        )}
+
+        <Button type="submit" className="!mt-8 w-full">
+          Thêm sản phẩm
+        </Button>
+      </form>
+    </Form>
+  );
+}
